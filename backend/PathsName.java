b1b6fd88b69fa2d9d978ed7167158fbf93801edd
bokade <121025630+bokade@users.noreply.github.com>
2023-08-17 18:27:17 +0530

    Create PathsName.java

---
+++ backend/PathsName.java
@@ -0,0 +1,9 @@
+
+package in.sp.paths;
+
+
+public class PathsName 
+{
+    public static String profile_pic_path = "C:\\Users\\admin\\Documents\\NetBeansProjects\\SocialMediaProject\\build\\web\\profile-pics\\";
+    public static String status_pic_path = "C:\\Users\\admin\\Documents\\NetBeansProjects\\SocialMediaProject\\build\\web\\status-images\\";
+}
